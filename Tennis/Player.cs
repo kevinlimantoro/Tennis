283d91cd2bacc9a8c507c544a80679d88b8de73e
kevinlimantoro <vinlimantoro@gmail.com>
2017-11-03 17:41:19 +0800

    Tennis

---
+++ Tennis/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tennis
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public int Index = 0;
+        public int[] ScoreIndex = new int[4] { 0, 15, 30, 40 };
+        public string[] ScoreNameIndex = new string[4] { "Love", "Fifty", "Thirty", "Forty" };
+        public Player(string name) {
+            Name = name;
+            Index = 0;
+            Score = ScoreIndex[Index];
+        }
+        public void AddScore() {
+            Index++;
+            Score = ScoreIndex[Index > 3 ? 3 : Index];
+        }
+
+        public string GetScoreName() {
+            return ScoreNameIndex[Index];
+        }
+    }
+}
---
+++ Tennis/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tennis;
+
+namespace Tennis
+{
+    class Game
+    {
+        private static Player p1 = new Player("Player 1");
+        private static Player p2 = new Player("Player 2");
+        private static Player AdvantagePlayer = new Player("");
+        private static bool isDeuce = false;
+        static void Main(string[] args)
+        {
+            while (true) {
+                printScore();
+                //check which player score here p1 or p2
+                p1.AddScore();
+                setAdvantagePlayer();
+                checkDeuce();
+                isGotWinner();
+                break;
+            }
+        }
+
+        static void printScore() {
+            Console.WriteLine(p1.Name + " " + p1.GetScoreName() + " - " + p2.GetScoreName() + " " + p2.Name);
+        }
+
+        static void checkDeuce() {
+            isDeuce = p1.Index == p2.Index && p1.Index >= 2;
+        }
+
+        static void setAdvantagePlayer() {
+            AdvantagePlayer = p1.Index > p2.Index ? p1 : p2;
+        }
+
+        static bool isGotWinner() {
+            if (isDeuce)
+                return false;
+            return ((p1.Index - p2.Index >=2 && AdvantagePlayer.Name == p1.Name) || (p2.Index - p1.Index >= 2 && AdvantagePlayer.Name == p2.Name));
+        }
+    }
+}
